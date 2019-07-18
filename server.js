32a258d4a8e2dfbb9ec053928b00ddea2e83e182
unknown <sluground@gmail.com>
2019-07-18 16:04:41 -0400

    new

--- server.js
+++ server.js
@@ -1,125 +1,193 @@
-var express = require("express");
+var express = require('express');
 
-var logger = require("morgan");
+var bodyParser = require('body-parser');
 
-var mongoose = require("mongoose");
+var logger = require('morgan');
 
-var axios = require("axios");
+var mongoose = require('mongoose');
 
-var cheerio = require("cheerio");
+var path = require('path');
 
-// Require all models
-var db = require("./models");
+var Note = require('./models/Note.js');
 
-var PORT = 3000;
+var Article = require('./models/Article.js');
 
-// Initialize Express
-var app = express();
+var request = require('request');
 
-// Configure middleware
+var cheerio = require('cheerio');
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+// mongoose.Promise = Promise;
+// mongoose.connect("" , {
+// 	useMongoClient: true
+// });
 
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
-
-// Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
-// Routes
+var db = mongoose.connection;
 
-// A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
+var PORT = process.env.PORT || 3000;
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
+var app = express();
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
-    });
+app.use(express.static("public"));
 
-    // Send a message to the client
-    res.send("Scrape Complete");
-  });
+var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({
+	defaultLayout: "main",
+	partialsDir: path.join(__dirname, "/views/layouts/partials")
+}));
+app.set("view engine", "handlebars");
+
+db.on("error", function(error){
+	console.log("Mongoose Error: ", error);
 });
 
-// Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-  // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
+db.once("open", function(){
+	console.log("Mongoose connection successful.");
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
-    .populate("note")
-    .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
+app.get("/", function(req,res){
+	Article.find({"saved": false}).limit(20).exec(function(error,data){
+		var hbsObject = {
+			article: data
+		};
+		console.log(hbsObject);
+		res.render("home", hbsObject);
+	});
 });
 
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
+app.get("/saved", function(req,res){
+	Article.find({"saved": true}).populate("notes").exec(function(error, articles){
+		var hbsObject = {
+			article: articles
+		};
+		res.render("saved", hbsObject);
+	});
 });
 
-// Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+app.get("/scrape", function(req,res){
+	request("https://www.nytimes.com/", function(error,response, html){
+		var $ = cheerio.load(html);
+		$("article").each(function(i,element){
+			var result = {};
+			result.title = $(this).children("h2").text();
+			result.summary = $(this).children(".summary").text();
+			result.link = $(this).children("h2").children("a").attr("href");
+
+			var entry = new Article(result);
+
+			entry.save(function(err, doc){
+				if(err){
+					console.log(err);
+				}
+				else{
+					console.log(doc);
+				}
+			});
+		});
+		res.send("Scrape Complete");
+	});
+});
+
+app.get("/articles", function(req,res){
+	Article.find({}).limit(20).exec(function(error, doc){
+		if(error){
+			console.log(error);
+		}
+		else{
+			res.json(doc);
+		}
+	});
+});
+
+app.get("/articles/:id", function(req,res){
+	Article.findOne({ "_id": req.params.id})
+	.populate("note")
+	.exec(function(error, doc){
+		if(error){
+			console.log(error);
+		}
+		else{
+			res.json(doc);
+		}
+	});
+});
+
+app.post("/articles/save/:id", function(req,res){
+	Article.findOneAndUpdate({ "_id": req.params.id}, {"saved": true})
+	.exec(function(err, doc){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.send(doc);
+		}
+	});
+});
+
+app.post("/articles/delete/:id", function(req,res){
+	Article.findOneAndUpdate({ "_id": req.params.id}, {"saved": false, "notes":[]})
+	.exec(function(err, doc){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.send(doc);
+		}
+	});
+});
+
+app.post("notes/save/:id", function(req,res){
+	var newNote = new Note({
+		body: req.body.text,
+		article: req.params.id
+	});
+	console.log(req.body)
+	newNote.save(function(error, note){
+		if(error){
+			console.log(error);
+		}
+		else{
+			Article.findOneAndUpdate({ "_id": req.params.id}, {$push: { "notes": note } })
+			.exec(function(err){
+				if(err){
+					console.log(err);
+					res.send(err);
+				}
+				else{
+					res.send(note);
+				}
+			});
+		}
+	});
+});
+
+app.delete("/notes/delete/:note_id/:article", function(req,res){
+	Note.findOneAndRemove({"_id": req.params.note.id}, function(err){
+		if(err){
+			console.log(err);
+			res.send(err);
+		}
+		else{
+			Article.findOneAndUpdate({"_id": req.params.article_id}, {$pull: {"notes": req.params.note_id}})
+				.exec(function(err){
+					if(err){
+						console.log(err);
+						res.send(err); 
+					}
+					else{
+						res.send("Note Deleted");
+					}
+				});
+		}
+	});
+});
+
+app.listen(PORT, function(){
+	console.log("App running on PORT: " + PORT);
 });
